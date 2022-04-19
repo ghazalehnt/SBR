24a97bf07d0b623d147ff88a97440d922ca0d44d
Ghazaleh H. Torbati <ghazaleh@mpi-inf.mpg.de>
2022-04-19 10:11:22 +0200

    small changes in config

--- SBR/model/vanilla_classifier.py
+++ SBR/model/vanilla_classifier.py
@@ -1,7 +1,7 @@
 import torch
 import transformers
 
-from SBR.utils.statics import INTERNAL_USER_ID_FIELD
+from SBR.utils.statics import INTERNAL_USER_ID_FIELD, INTERNAL_ITEM_ID_FIELD
 
 
 class VanillaClassifierUserTextProfileItemTextProfile(torch.nn.Module):
@@ -16,6 +16,7 @@ class VanillaClassifierUserTextProfileItemTextProfile(torch.nn.Module):
         if config["append_id"]:
             self.user_id_embedding = torch.nn.Embedding(n_users, bert_embedding_dim)
             self.item_id_embedding = torch.nn.Embedding(n_items, bert_embedding_dim)
+            self.bert_embeddings = self.bert.get_input_embeddings()
 
         if "k" in config:
             self.transform_u = torch.nn.Linear(bert_embedding_dim, config['k'])
@@ -28,11 +29,55 @@ class VanillaClassifierUserTextProfileItemTextProfile(torch.nn.Module):
         self.chunk_agg_strategy = config['chunk_agg_strategy']
 
     def forward(self, batch):
-        # if hasattr(self, 'user_id_embedding'):
-        #     pass
-        # else:
-        if True:
-            # batch -> chunks * batch_size * tokens
+        # batch -> chunks * batch_size * tokens
+        if hasattr(self, 'bert_embeddings'):  # we want to append an id embedding to the tokens
+            # user:
+            user_ids = batch[INTERNAL_USER_ID_FIELD]
+            id_embeddings = self.user_id_embedding(user_ids)
+            outputs = []
+            # go over chunks:
+            for input_ids, att_mask in zip(
+                    batch['user_chunks_input_ids'], batch['user_chunks_attention_mask']):
+                token_embeddings = self.bert_embeddings.forward(input_ids)
+                cls_tokens = token_embeddings[:, 0].unsqueeze(1)
+                other_tokens = token_embeddings[:, 1:]
+                # insert user_id embedding after the especial CLS token:
+                concat_ids = torch.concat([torch.concat([cls_tokens, id_embeddings], dim=1), other_tokens], dim=1)
+                concat_masks = torch.concat([torch.ones((2, 1), device=att_mask.device), att_mask], dim=1)
+                output = self.bert.forward(inputs_embeds=concat_ids,
+                                           attention_mask=concat_masks)
+                if self.agg_strategy == "CLS":
+                    temp = output.pooler_output
+                elif self.agg_strategy == "mean":
+                    raise ValueError("not implemented yet")
+                else:
+                    raise ValueError(f"agg_strategy not implemented {self.agg_strategy}")
+                outputs.append(temp)
+            user_rep = torch.stack(outputs).max(dim=0).values
+            # item:
+            item_ids = batch[INTERNAL_ITEM_ID_FIELD]
+            id_embeddings = self.item_id_embedding(item_ids)
+            outputs = []
+            # go over chunks:
+            for input_ids, att_mask in zip(
+                    batch['user_chunks_input_ids'], batch['user_chunks_attention_mask']):
+                token_embeddings = self.bert_embeddings.forward(input_ids)
+                cls_tokens = token_embeddings[:, 0].unsqueeze(1)
+                other_tokens = token_embeddings[:, 1:]
+                # insert user_id embedding after the especial CLS token:
+                concat_ids = torch.concat([torch.concat([cls_tokens, id_embeddings], dim=1), other_tokens], dim=1)
+                concat_masks = torch.concat([torch.ones((2, 1), device=att_mask.device), att_mask], dim=1)
+                output = self.bert.forward(inputs_embeds=concat_ids,
+                                           attention_mask=concat_masks)
+                if self.agg_strategy == "CLS":
+                    temp = output.pooler_output
+                elif self.agg_strategy == "mean":
+                    raise ValueError("not implemented yet")
+                else:
+                    raise ValueError(f"agg_strategy not implemented {self.agg_strategy}")
+                outputs.append(temp)
+            item_rep = torch.stack(outputs).max(dim=0).values
+        else:
             # user:
             outputs = []
             for input_ids, att_mask in zip(batch['user_chunks_input_ids'], batch['user_chunks_attention_mask']):
