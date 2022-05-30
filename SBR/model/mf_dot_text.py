1a9eb79d30cf670aaabc51086d0ace70192e5010
Ghazaleh H. Torbati <ghazaleh@mpi-inf.mpg.de>
2022-05-30 14:15:07 +0200

    adding new model combining cf and text by concat

---
+++ SBR/model/mf_dot_text.py
@@ -0,0 +1,62 @@
+import torch
+
+import os
+
+from SBR.utils.statics import INTERNAL_USER_ID_FIELD, INTERNAL_ITEM_ID_FIELD
+
+
+class MatrixFactorizatoinTextDotProduct(torch.nn.Module):
+    def __init__(self, config, n_users, n_items, device, prec_dir):
+        super(MatrixFactorizatoinTextDotProduct, self).__init__()
+
+        self.user_embedding = torch.nn.Embedding(n_users, config["embedding_dim"])
+        self.item_embedding = torch.nn.Embedding(n_items, config["embedding_dim"])
+
+        self.user_bias = torch.nn.Parameter(torch.zeros(n_users))
+        self.item_bias = torch.nn.Parameter(torch.zeros(n_items))
+        self.bias = torch.nn.Parameter(torch.zeros(1))
+
+        bert_embedding_dim = 768
+        if "k" in config and config["k"] not in ['', 0]:
+            self.transform_u_text = torch.nn.Linear(bert_embedding_dim, config['k'])
+            self.transform_i_text = torch.nn.Linear(bert_embedding_dim, config['k'])
+        freeze_prec_rep = config['freeze_prec_reps']
+        agg_strategy = config['agg_strategy']
+        chunk_agg_strategy = config['chunk_agg_strategy']
+        user_rep_file = f"{agg_strategy}_{chunk_agg_strategy}_" \
+                        f"id{config['append_id']}_tb{config['tune_BERT']}_cf{config['use_CF']}_user_representation.pkl"
+        if os.path.exists(os.path.join(prec_dir, user_rep_file)):
+            weights = torch.load(os.path.join(prec_dir, user_rep_file), map_location=device)
+        else:
+            raise ValueError(f"Precalculated user embedding does not exist! {os.path.join(prec_dir, user_rep_file)}")
+        self.user_text_rep = torch.nn.Embedding.from_pretrained(weights, freeze=freeze_prec_rep)
+
+        item_rep_file = f"{agg_strategy}_{chunk_agg_strategy}_" \
+                        f"id{config['append_id']}_tb{config['tune_BERT']}_cf{config['use_CF']}_item_representation.pkl"
+        if os.path.exists(os.path.join(prec_dir, item_rep_file)):
+            weights = torch.load(os.path.join(prec_dir, item_rep_file), map_location=device)
+        else:
+            raise ValueError(f"Precalculated item embedding does not exist! {os.path.join(prec_dir, item_rep_file)}")
+        self.item_text_rep = torch.nn.Embedding.from_pretrained(weights, freeze=freeze_prec_rep)
+
+    def forward(self, batch):
+        user_ids = batch[INTERNAL_USER_ID_FIELD].squeeze()
+        item_ids = batch[INTERNAL_ITEM_ID_FIELD].squeeze()
+
+        user_embeds = self.user_embedding(user_ids)
+        item_embeds = self.item_embedding(item_ids)
+
+        user_t_rep = self.user_text_rep(user_ids)
+        item_t_rep = self.item_text_rep(item_ids)
+        if hasattr(self, 'transform_u_text'):
+            user_t_rep = self.transform_u_text(user_t_rep)
+            item_t_rep = self.transform_i_text(item_t_rep)
+
+        user_rep = torch.concat([user_embeds, user_t_rep], dim=1)
+        item_rep = torch.concat([item_embeds, item_t_rep], dim=1)
+
+        output = torch.sum(torch.mul(user_rep, item_rep), dim=1)
+        output = output + self.item_bias[item_ids] + self.user_bias[user_ids]
+        output = output + self.bias
+
+        return output.unsqueeze(1)  # do not apply sigmoid and use BCEWithLogitsLoss
--- SBR/utils/others.py
+++ SBR/utils/others.py
@@ -1,6 +1,7 @@
 from SBR.model.mf_dot import MatrixFactorizatoinDotProduct
 from SBR.model.vanilla_classifier import VanillaClassifierUserTextProfileItemTextProfile
 from SBR.model.vanilla_classifier_precalc_representations import VanillaClassifierUserTextProfileItemTextProfilePrecalculated
+from mf_dot_text import MatrixFactorizatoinTextDotProduct
 
 
 def get_model(config, user_info, item_info, n_classes=None, padding_token=None, device=None, prec_dir=None):
@@ -10,13 +11,20 @@ def get_model(config, user_info, item_info, n_classes=None, padding_token=None, 
         model = VanillaClassifierUserTextProfileItemTextProfile(config=config, n_users=user_info.shape[0],
                                                                 n_items=item_info.shape[0], num_classes=n_classes)
     elif config['name'] == "VanillaClassifier_precalc":
-        model = VanillaClassifierUserTextProfileItemTextProfilePrecalculated(config=config, n_users=user_info.shape[0],
+        model = VanillaClassifierUserTextProfileItemTextProfilePrecalculated(config=config,
+                                                                             n_users=user_info.shape[0],
                                                                              n_items=item_info.shape[0],
                                                                              num_classes=n_classes,
                                                                              user_info=user_info, item_info=item_info,
                                                                              padding_token=padding_token,
                                                                              device=device,
                                                                              prec_dir=prec_dir)
+    elif config['name'] == "MF_TEXT_DOT":
+        model = MatrixFactorizatoinTextDotProduct(config=config,
+                                                  n_users=user_info.shape[0],
+                                                  n_items=item_info.shape[0],
+                                                  device=device,
+                                                  prec_dir=prec_dir)
     else:
         raise ValueError(f"Model is not implemented! model.name = {config['name']}")
     return model
