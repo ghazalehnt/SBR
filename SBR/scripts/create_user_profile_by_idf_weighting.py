37a69c80451f0d6be186fa5ef4a9b7a34df626b2
Ghazaleh H. Torbati <ghazaleh@mpi-inf.mpg.de>
2022-06-15 14:28:02 +0200

    filtering user text by idf weights

---
+++ SBR/scripts/create_user_profile_by_idf_weighting.py
@@ -0,0 +1,76 @@
+# we want to identify the most informative stuff by idf value.  this script does not care about book description.
+# 3 granularity:
+# 1- words
+# 2- phrases, n-grams
+# 3- sentences (accumulate high idf weights).  -> length normalization?
+## 2 methods: idf threshold, or top-k highest
+## note: probably need to rescale idf weights as they are very small, e.g. exp(idf) todo exp(idf)=exp(log(N/df))=N/df
+### note: how about filtering by tf also? to avoid very rare workds???
+import json
+from os.path import join
+
+import torch
+import numpy as np
+
+from torchtext.data.utils import get_tokenizer
+
+from SBR.utils.data_loading import load_crawled_goodreads_dataset
+from SBR.utils.get_idf_weights_googlengrams import get_idf_weights
+
+
+def tokenize_function_torchtext(samples, tokenizer=None, doc_desc_field="text"):
+    tokenized_batch = {}
+    tokenized_batch[f"tokenized_{doc_desc_field}"] = [tokenizer(text) for text in samples[doc_desc_field]]
+    return tokenized_batch
+
+
+def tokens_idf_weights(samples, idf_weights=None):
+    enriched_batch = {}
+    enriched_batch['idf'] = [[idf_weights[token] if token in idf_weights else 0 for token in tokens] for tokens in samples['tokenized_text']]
+    return enriched_batch
+
+
+def choose_topk(samples, k=100):
+    filtered_batch = {}
+    filtered_batch['filtered_text_tokens'] = [list(np.array(tokens)[np.argpartition(weights, -1 * min(len(weights), k))[-1 * min(len(weights), k):]]) for tokens, weights in zip(samples['tokenized_text'], samples['idf'])]
+    return filtered_batch
+
+
+def main(config_file):
+    np.random.seed(42)
+    torch.manual_seed(42)
+    torch.cuda.manual_seed(42)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    config = json.load(open(config_file, 'r'))
+    ### todo these are irrelevant to this script but check later:
+    if config['model']['precalc_batch_size'] > 1:
+        raise ValueError("There is a bug when the batch size is bigger than one. Users/items with only one chunk"
+                         "are producing wrong reps.")
+
+    if "<DATA_ROOT_PATH>" in config["dataset"]["dataset_path"]:
+        config["dataset"]["dataset_path"] = config["dataset"]["dataset_path"] \
+            .replace("<DATA_ROOT_PATH>", open("data/paths_vars/DATA_ROOT_PATH").read().strip())
+
+    datasets, user_info, item_info = load_crawled_goodreads_dataset(config['dataset'])
+
+    # tokenizer = get_tokenizer("basic_english")
+    tokenizer = get_tokenizer("spacy")
+    user_info = user_info.map(tokenize_function_torchtext, fn_kwargs={'tokenizer': tokenizer}, batched=True)
+
+    # we need the vocab to get only the correspinding ngrams
+    vocab = []
+    for tokens in user_info['tokenized_text']:
+        vocab.extend(tokens)
+    vocab = set(vocab)
+    # TODO other params?
+    idf_weights = get_idf_weights(config['dataset']['googlengram_path'],
+                                  config['dataset']['user_text_filter_granularity'],
+                                  vocab, False, False)
+    user_info = user_info.map(tokens_idf_weights, fn_kwargs={'idf_weights': idf_weights}, batched=True)
+    # now filter ... top k highest idf weighted terms?
+    k = 100  # todo get from config
+    user_info = user_info.map(choose_topk, fn_kwargs={'k': k}, batched=True)
+    user_info.to_csv(join(config['dataset']['dataset_path'], f"filtered_user_info_idf_top{k}.csv"))
+
+# main("configs/example_dataset/precalc_toy.json")
---
+++ SBR/utils/get_idf_weights_googlengrams.py
@@ -0,0 +1,58 @@
+from os import listdir
+from os.path import join
+import gzip
+
+import numpy as np
+
+
+def idf(N, df, smooth, prob_idf):
+    if smooth and prob_idf:
+        raise ValueError("Not implemented")
+    elif smooth:
+        raise ValueError("Not implemented")
+    elif prob_idf:
+        raise ValueError("Not implemented")
+    else:
+        return np.log10(N/df)
+
+# total doc num is in the totalcount... match_count, page_count, volume_count
+def acc_num_docs(per_year_stat):
+    return acc_df_weights(per_year_stat, field=3)
+
+
+def acc_df_weights(per_year_stat, field=2):
+    #year,total_term_count,document_count
+    df = 0
+    for st in per_year_stat:
+        sp = st.split(',')
+        df += int(sp[field])
+    return df
+
+
+def get_idf_weights(ngram_dir, n, keys, idf_smooth, idf_prob):
+    idf_weights = {}
+
+    # first read total number of documents from totalcount file:
+    total_num_docs = 0
+    with open(join(ngram_dir, f'{n}-grams', f'totalcounts-{n}'), 'r') as f:
+        for line in f:# only one line
+            sp = line.strip().split("\t")
+            total_num_docs += acc_num_docs(sp)
+
+    files = listdir(join(ngram_dir, f'{n}-grams'))
+    for f in files:
+        if f.startswith('totalcounts') or f == 'download.sh':
+            continue
+        with gzip.open(join(ngram_dir, f'{n}-grams', f), 'rt') as f:
+            for line in f:
+                sp = line.strip().split("\t")
+                k = sp[0]
+                if keys is None or k in keys:
+                    df = acc_df_weights(sp[1:])
+                    idf_weights[k] = idf(total_num_docs, df, idf_smooth, idf_prob)
+    return idf_weights
+
+
+#
+# idfs = get_idf_weights("PATH/GoogleNgrams/", 1, None, False, False)
+# print(idfs)
