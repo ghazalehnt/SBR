a8080c62bf3375875d522596a8c5441a40756dce
Ghazaleh H. Torbati <ghazaleh@mpi-inf.mpg.de>
2022-07-25 23:56:49 +0200

    implement year_constraint to calc idf, case insensitive

--- SBR/utils/get_idf_weights_googlengrams.py
+++ SBR/utils/get_idf_weights_googlengrams.py
@@ -15,21 +15,34 @@ def idf(N, df, smooth, prob_idf):
     else:
         return np.log10(N/df)
 
-# total doc num is in the totalcount... match_count, page_count, volume_count
-def acc_num_docs(per_year_stat):
-    return acc_df_weights(per_year_stat, field=3)
+# total doc num is in the totalcount... year, match_count, page_count, volume_count
+def acc_num_docs(per_year_stat, year_const):
+    return acc_df_weights(per_year_stat, year_const, field=3)
 
 
-def acc_df_weights(per_year_stat, field=2):
+def acc_df_weights(per_year_stat, year_const, field=2):
+    from_year = None
+    # if year_const.startswith('last_'):
+    #     per_year_stat = per_year_stat[-1 * int(year_const[5:])]
+    if year_const.startswith('from_'):
+        from_year = int(year_const[5:])
+
     #year,total_term_count,document_count
     df = 0
     for st in per_year_stat:
         sp = st.split(',')
+        if from_year is not None:
+            if int(sp[0]) < from_year:
+                continue
         df += int(sp[field])
     return df
 
 
-def get_idf_weights(ngram_dir, n, keys, idf_smooth, idf_prob):
+def get_idf_weights(ngram_dir, n, keys, idf_smooth, idf_prob, case_sensitive=True, year_const='all'):
+    if year_const != 'all' and not year_const.startswith('from_'):
+        raise ValueError(f"year_const = {year_const} not implemented")
+
+    acc_df = {}
     idf_weights = {}
 
     # first read total number of documents from totalcount file:
@@ -37,7 +50,7 @@ def get_idf_weights(ngram_dir, n, keys, idf_smooth, idf_prob):
     with open(join(ngram_dir, f'{n}-grams', f'totalcounts-{n}'), 'r') as f:
         for line in f:# only one line
             sp = line.strip().split("\t")
-            total_num_docs += acc_num_docs(sp)
+            total_num_docs += acc_num_docs(sp, year_const)
 
     files = listdir(join(ngram_dir, f'{n}-grams'))
     for f in files:
@@ -45,14 +58,27 @@ def get_idf_weights(ngram_dir, n, keys, idf_smooth, idf_prob):
             continue
         with gzip.open(join(ngram_dir, f'{n}-grams', f), 'rt') as f:
             for line in f:
-                sp = line.strip().split("\t")
+                sp = line.strip().split("\t")  ### TODO maybe space is better than specifying \t? but gor 2,3 grams should concat them again...
                 k = sp[0]
+                if case_sensitive is False:
+                    k = k.lower()
+                    if k.startswith('pancaky'):
+                        print(sp)
+                if k not in acc_df:
+                    acc_df[k] = 0
                 if keys is None or k in keys:
-                    df = acc_df_weights(sp[1:])
-                    idf_weights[k] = idf(total_num_docs, df, idf_smooth, idf_prob)
+                    df = acc_df_weights(sp[1:], year_const)
+                    acc_df[k] += df
+    for k, df in acc_df.items():
+        if df > 0:
+            idf_weights[k] = idf(total_num_docs, df, idf_smooth, idf_prob)
     return idf_weights
 
 
-#
-# idfs = get_idf_weights("PATH/GoogleNgrams/", 1, None, False, False)
+idfs = get_idf_weights("PATH/GoogleNgrams/", 1, None, False, False, year_const='from_1980',
+                       case_sensitive=False)
 # print(idfs)
+
+# implemented all, from_year
+# note: I also implemented last_year to get only the last years of a word, however, it is problematic as
+# we cannot calculate the total docs to calculate the idf, as the last_x differs for each term.
