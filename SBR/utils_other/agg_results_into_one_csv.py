6c984a36df41f120046972012e6cbcb5927ff829
Ghazaleh H. Torbati <ghazaleh@mpi-inf.mpg.de>
2022-08-24 00:11:22 +0200

    more info from fname, round numbers

--- SBR/utils_other/agg_results_into_one_csv.py
+++ SBR/utils_other/agg_results_into_one_csv.py
@@ -1,22 +1,58 @@
 import argparse
 import csv
+import math
 from os import listdir
 from os.path import join
 
 
+def round_half_up(n, decimals=0):
+    multiplier = 10 ** decimals
+    return math.floor(n * multiplier + 0.5) / multiplier
+
+# False_200_dot_product_0.0004_1e-08_256_4096_random_4_f:validation_neg_random_100_f:test_neg_random_100_1_1_512_max_pool_mean_last__False_True_item.avg_rating_interaction.review
+# False_200_dot_product_0.0004_1e-08_256_4096_random_4_f:validation_neg_random_100_f:test_neg_random_100_1_1_512_max_pool_mean_last__False_True_item.avg_rating_item.title-item.genres,Test
+# .avg_rating_item.title-item.genres-interaction.review,Test
 def main(exp_dir, result_file_name):
     rows = []
     for fname in sorted(listdir(exp_dir)):
         print(fname)
+        if fname.startswith("False") or fname.startswith("True"):
+            if "test_neg_random_100_1_1_" in fname:
+                ch = 1
+            elif "test_neg_random_100_5_5_" in fname:
+                ch = 5
+            else:
+                raise ValueError("num chunks not found in fname")
+            sortby = ""
+            for filter in ["tf-idf_1", "tf-idf_2", "tf-idf_3", "tf-idf_1-2-3", "idf_1_unique", "idf_2_unique",
+                           "idf_3_unique", "idf_1-2-3_unique", "idf_sentence"]:
+                if filter in fname:
+                    sortby = filter
+                    break
+            profile = []
+            if "item.title" in fname:
+                profile.append('t')
+            if "item.genres" in fname:
+                profile.append('g')
+            if "interaction.review" in fname:
+                profile.append('r')
+            profile = ''.join(profile)
+            if fname.startswith("True"):
+                model_config = f"CF-BERT_{ch}CH_{sortby}"
+            else:
+                model_config = f"BERT_{ch}CH_{sortby}"
+        else:
+            model_config = f"CF-{fname[:fname.index('_')]}"
+            profile = ''
         res_file = open(join(exp_dir, fname, result_file_name), 'r')
         reader = csv.reader(res_file)
         header = next(reader)
         for line in reader:
-            rows.append([fname] + line)
+            rows.append([model_config, profile] + [str(round_half_up(float(m)*100, 4)) for m in line])
         rows.append([])
 
-    header = ["config"] + header
-    with open(join(args.dir, "test_results.csv")) as outfile:
+    header = ["model config", "user profile"] + header
+    with open(join(args.dir, "test_results.csv"), 'w') as outfile:
         print(join(args.dir, "test_results.csv"))
         writer = csv.writer(outfile)
         writer.writerow(header)
@@ -26,7 +62,7 @@ def main(exp_dir, result_file_name):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--dir', '-d', type=str, default=None, help='experiments dir')
-    parser.add_argument('--thresholds', type=int, nargs='+', default=None, help='user thresholds')
+    parser.add_argument('--thresholds', '-t', type=int, nargs='+', default=None, help='user thresholds')
     parser.add_argument('--set', '-s', type=str, default='test', help='valid/test')
     args, _ = parser.parse_known_args()
 
