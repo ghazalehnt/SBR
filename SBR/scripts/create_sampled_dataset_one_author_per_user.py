652feffaf6e589c4974ff854961a929bb5396d78
Ghazaleh H. Torbati <ghazaleh@mpi-inf.mpg.de>
2022-09-29 16:28:52 +0200

    supporting GR data, cleaning

--- SBR/scripts/create_sampled_dataset_one_author_per_user.py
+++ SBR/scripts/create_sampled_dataset_one_author_per_user.py
@@ -35,23 +35,25 @@ if __name__ == "__main__":
     random.seed(42)
     np.random.seed(42)
 
-    # DATASET_PATH = ".../extracted_dataset..."
-    # INTERACTION_FILE = "goodreads_crawled.interactions"
-    # ITEM_FILE = "goodreads_crawled.items"
-    # USER_FILE = "goodreads_crawled.users"
-    # USER_ID_FIELD = "user_id"
-    # ITEM_ID_FIELD = "item_id"
-    # RATING_FIELD = "rating"
-
     DATASET_PATH = ""
-    INTERACTION_FILE = "amazon_reviews_books.interactions"
-    ITEM_FILE = "amazon_reviews_books.items"
-    USER_FILE = "amazon_reviews_books.users"
-    USER_ID_FIELD = "reviewerID"
-    ITEM_ID_FIELD = "asin"
-    RATING_FIELD = "overall"
-    AUTHOR_FIELD = "brand"
-    TIE_BREAK_FIELD = "rank"
+    INTERACTION_FILE = "goodreads_crawled.interactions"
+    ITEM_FILE = "goodreads_crawled.items"
+    USER_FILE = "goodreads_crawled.users"
+    USER_ID_FIELD = "user_id"
+    ITEM_ID_FIELD = "item_id"
+    RATING_FIELD = "rating"
+    AUTHOR_FIELD = "author"
+    TIE_BREAK_FIELD = "avg_rating"
+
+    # DATASET_PATH = ""
+    # INTERACTION_FILE = "amazon_reviews_books.interactions"
+    # ITEM_FILE = "amazon_reviews_books.items"
+    # USER_FILE = "amazon_reviews_books.users"
+    # USER_ID_FIELD = "reviewerID"
+    # ITEM_ID_FIELD = "asin"
+    # RATING_FIELD = "overall"
+    # AUTHOR_FIELD = "brand"
+    # TIE_BREAK_FIELD = "rank"
 
     rating_threshold = 3
 
@@ -62,6 +64,8 @@ if __name__ == "__main__":
     all_authors = set()
     if INTERACTION_FILE.startswith("amazon_reviews_books"):
         item_tie_breaks = defaultdict(lambda: {0: math.inf, 1: math.inf, 2: math.inf, 3: math.inf})
+    elif INTERACTION_FILE.startswith("train"): # TODO
+        item_tie_breaks = defaultdict(lambda: 0)
     else:
         raise NotImplementedError("todo")
     with open(join(DATASET_PATH, ITEM_FILE), 'r') as f:
@@ -78,39 +82,44 @@ if __name__ == "__main__":
             if author == "":
                 continue
             item_authors[item_id] = author
-            tie_brk = line[TIE_BREAK_IDX_ITEM]
-            # todo adapt for goodreads for avg rating
-            if tie_brk in ["", "[]"]:
-                tie_brk = 0
-            elif "in" in tie_brk:
-                # in Books
-                try:
-                    tie_brk = int(re.sub(r"(?:\['>#)?([\d,]+) in Books.*", '\g<1>', tie_brk).replace(',', ''))
-                    item_tie_breaks[item_id][0] = tie_brk
-                except:
+            tie_brk_line = line[TIE_BREAK_IDX_ITEM]
+            if INTERACTION_FILE.startswith("amazon_reviews_books"):
+                if tie_brk_line in ["", "[]"]:
+                    pass
+                elif "in" in tie_brk_line:
+                    # in Books
                     try:
-                        tie_brk = int(re.sub(r"(?:\['>#)?([\d,]+) Paid in Kindle Store.*", '\g<1>', tie_brk).replace(',', ''))
-                        item_tie_breaks[item_id][1] = tie_brk
+                        tie_brk = int(re.sub(r"(?:\['>#)?([\d,]+) in Books.*", '\g<1>', tie_brk_line).replace(',', ''))
+                        item_tie_breaks[item_id][0] = tie_brk
                     except:
                         try:
                             tie_brk = int(
-                                re.sub(r"(?:\['>#)?([\d,]+) Free in Kindle Store.*", '\g<1>', tie_brk).replace(',', ''))
-                            item_tie_breaks[item_id][2] = tie_brk
+                                re.sub(r"(?:\['>#)?([\d,]+) Paid in Kindle Store.*", '\g<1>', tie_brk_line).replace(',', ''))
+                            item_tie_breaks[item_id][1] = tie_brk
                         except:
                             try:
                                 tie_brk = int(
-                                    re.sub(r"(?:\['>#)?([\d,]+) in .*", '\g<1>', tie_brk).replace(',',
-                                                                                                                   ''))
-                                item_tie_breaks[item_id][3] = tie_brk
+                                    re.sub(r"(?:\['>#)?([\d,]+) Free in Kindle Store.*", '\g<1>', tie_brk_line).replace(',', ''))
+                                item_tie_breaks[item_id][2] = tie_brk
                             except:
-                                print(tie_brk)
-                                tie_brk = 0
-            else:
-                print(tie_brk)
-                tie_brk = 0
+                                try:
+                                    tie_brk = int(
+                                        re.sub(r"(?:\['>#)?([\d,]+) in .*", '\g<1>', tie_brk_line).replace(',', ''))
+                                    item_tie_breaks[item_id][3] = tie_brk
+                                except:
+                                    print(tie_brk_line)
+                                    pass
+                else:
+                    # print(tie_brk_line)
+                    pass
+            elif INTERACTION_FILE.startswith("train"):
+                if tie_brk_line in [""]:
+                    pass
+                else:
+                    item_tie_breaks[item_id] = float(tie_brk_line)
             all_authors.add(author)
 
-    all_interactions_per_user_per_author = defaultdict(lambda : defaultdict(list))
+    all_interactions_per_user_per_author = defaultdict(lambda: defaultdict(list))
     source_interactions_cnt = 0
     no_author_inters_cnt = 0
     with open(join(DATASET_PATH, INTERACTION_FILE), 'r') as f:
@@ -126,7 +135,7 @@ if __name__ == "__main__":
                 no_author_inters_cnt += 1
                 continue
             if rating_threshold is not None:
-                if INTERACTION_FILE.startswith("goodreads_crawled"):
+                if INTERACTION_FILE.startswith("train"):
                     rating = rating_mapping[line[RATING_IDX_INTER]]
                 elif INTERACTION_FILE.startswith("amazon_reviews_books"):
                     rating = int(float(line[RATING_IDX_INTER]))
@@ -150,12 +159,17 @@ if __name__ == "__main__":
                 remaining_items.add(inters[0][ITEM_ID_IDX_INTER])
             else:
                 # choosing 1 book per author per user
-                inters = sorted(inters, key=lambda x: (-1 * int(float(x[RATING_IDX_INTER])),
-                                                       item_tie_breaks[x[ITEM_ID_IDX_INTER]][0],
-                                                       item_tie_breaks[x[ITEM_ID_IDX_INTER]][1],
-                                                       item_tie_breaks[x[ITEM_ID_IDX_INTER]][2],
-                                                       item_tie_breaks[x[ITEM_ID_IDX_INTER]][3]),
+                if INTERACTION_FILE.startswith("amazon_reviews_books"):
+                    inters = sorted(inters, key=lambda x: (-1 * int(float(x[RATING_IDX_INTER])),
+                                                           item_tie_breaks[x[ITEM_ID_IDX_INTER]][0],
+                                                           item_tie_breaks[x[ITEM_ID_IDX_INTER]][1],
+                                                           item_tie_breaks[x[ITEM_ID_IDX_INTER]][2],
+                                                           item_tie_breaks[x[ITEM_ID_IDX_INTER]][3]),
                                 reverse=False)  # main sort is rating, bigger the better. but we *-1 to be coherent with other tie breakers which is the rank, smaller better.
+                elif INTERACTION_FILE.startswith("train"):
+                    inters = sorted(inters, key=lambda x: (rating_mapping[x[RATING_IDX_INTER]],
+                                                           item_tie_breaks[x[ITEM_ID_IDX_INTER]]),
+                                    reverse=True)  # for both rating and avg-rating bigger better
                 interactions.append(inters[0])
                 remaining_items.add(inters[0][ITEM_ID_IDX_INTER])
 
