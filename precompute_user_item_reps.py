9aefc5ec3faec6d6f24f41ca8682148402028ffb
Ghazaleh H. Torbati <ghazaleh@mpi-inf.mpg.de>
2022-10-05 23:13:31 +0200

    cleaning

--- precompute_user_item_reps.py
+++ precompute_user_item_reps.py
@@ -80,8 +80,8 @@ def main(config_file, given_user_text_filter=None, given_limit_training_data=Non
                     f"cf{config['model']['use_CF']}_" \
                     f"ch{config['dataset']['max_num_chunks_user']}_" \
                     f"{'-'.join(config['dataset']['user_text'])}_" \
-                    f"{config['dataset']['user_review_choice']}_" \
-                    f"{config['dataset']['review_tie_breaker'] if config['dataset']['user_text_filter'] not in ['', 'item_sentence_SBERT'] else ''}_" \
+                    f"{config['dataset']['user_item_text_choice']}_" \
+                    f"{config['dataset']['user_item_text_tie_breaker'] if config['dataset']['user_text_filter'] not in ['', 'item_sentence_SBERT'] else ''}_" \
                     f"{config['dataset']['user_text_filter'] if len(config['dataset']['user_text_filter']) > 0 else 'no-filter'}" \
                     f"{'_i'+'-'.join(config['dataset']['item_text']) if config['dataset']['user_text_filter'] in ['item_sentence_SBERT'] else ''}" \
                     f".pkl"
