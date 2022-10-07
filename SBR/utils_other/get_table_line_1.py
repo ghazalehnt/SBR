e5967d938732b505107ad54dfe27c8e1d12530ae
Ghazaleh H. Torbati <ghazaleh@mpi-inf.mpg.de>
2022-10-07 15:44:42 +0200

    helper script

---
+++ SBR/utils_other/get_table_line_1.py
@@ -0,0 +1,39 @@
+import argparse
+from collections import defaultdict
+
+import pandas as pd
+
+def main(res_file, given_user_group, given_metric):
+    res = pd.read_csv(res_file)
+    res = res.fillna("")
+    print(res_file)
+    print(given_user_group)
+    print(given_metric)
+    lines = defaultdict(lambda: defaultdict())
+    for model, user_text, item_text, group, metric in zip(res['model config'], res['user profile'], res['item text'], res['group'], res[given_metric]):
+        if given_user_group not in group:
+            continue
+        if model.startswith("CF"):
+            print(f"{given_user_group}-> {model} & {metric} & & & \\ \hline")
+        elif model in ["BERT_1CH_", "BERT_5CH_", "BERT_1CH_item_sentence_SBERT", "BERT_1CH_idf_sentence", "BERT_1CH_tf-idf_3"]:
+            if item_text in ["tc", "tg"]:
+                if user_text in ["tc", "tg"]:
+                    lines[model][1] = metric
+                elif user_text in ["tcsr", "tgr"]:
+                    lines[model][3] = metric
+            elif item_text in ["tcd", "tgd"]:
+                if user_text in ["tc", "tg"]:
+                    lines[model][2] = metric
+                elif user_text in ["tcsr", "tgr"]:
+                    lines[model][4] = metric
+    for model in lines.keys():
+        print(f"{given_user_group}-> {model} & {lines[model][1]} & {lines[model][2]} & {lines[model][3]} & {lines[model][4]} \\ \hline")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--res_file', '-r', type=str, required=True)
+    parser.add_argument('--user_group', '-g', type=str, default="group 1-5")
+    parser.add_argument('--report_metric', '-m', type=str, default="ndcg_cut_5")
+    args, _ = parser.parse_known_args()
+    main(args.res_file, args.user_group, args.report_metric)
