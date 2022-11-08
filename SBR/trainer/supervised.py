d7453cb1428d5880ac72b6718ac3862facbdebd4
Ghazaleh H. Torbati <ghazaleh@mpi-inf.mpg.de>
2022-11-08 11:55:44 +0100

    remove extra logging, calculate p1 and mmr again for explicit labels

--- SBR/trainer/supervised.py
+++ SBR/trainer/supervised.py
@@ -92,11 +92,12 @@ class SupervisedTrainer:
         # _, _, trloss, _, _ = self.predict(train_dataloader)
         # print(f"Train loss before training: {trloss:.8f}")
         #
-        outputs, ground_truth, valid_loss, users, items = self.predict(valid_dataloader)
-        results = calculate_metrics(ground_truth, outputs, users, items,
-                                    self.relevance_level, prediction_threshold=0.5, ranking_only=True)
-        results = {f"valid_{k}": v for k, v in results.items()}
-        print(f"Valid loss before training: {valid_loss:.8f} - {self.valid_metric} = {results[self.valid_metric]:.6f}")
+
+        # outputs, ground_truth, valid_loss, users, items = self.predict(valid_dataloader)
+        # results = calculate_metrics(ground_truth, outputs, users, items, self.relevance_level)
+        # results = {f"valid_{k}": v for k, v in results.items()}
+        # print(f"Valid loss before training: {valid_loss:.8f} - {self.valid_metric} = {results[self.valid_metric]:.6f}")
+
         # random.seed(42)
         # np.random.seed(42)
         # torch.manual_seed(42)
@@ -113,8 +114,8 @@ class SupervisedTrainer:
             train_loss, total_count = 0, 0
 
             # for loop going through dataset
-            tr_outputs = []
-            tr_labels = []
+            # tr_outputs = []
+            # tr_labels = []
             for batch_idx, batch in pbar:
                 # data preparation
                 batch = {k: v.to(self.device) for k, v in batch.items()}
@@ -127,8 +128,8 @@ class SupervisedTrainer:
                     # not applying sigmoid before loss bc it is already applied in the loss
                     loss = self.loss_fn(output, label)
                     # just apply sigmoid for logging
-                    tr_outputs.extend(list(torch.sigmoid(output)))
-                    tr_labels.extend(label)
+                    # tr_outputs.extend(list(torch.sigmoid(output.to('cpu'))))
+                    # tr_labels.extend(label.to('cpu'))
                 else:
                     if self.sig_output:
                         output = torch.sigmoid(output)
@@ -139,8 +140,8 @@ class SupervisedTrainer:
                         loss = self.loss_fn(pos_out, neg_out, pos_l)
                     else:
                         loss = self.loss_fn(output, label)
-                    tr_outputs.extend(list(output))
-                    tr_labels.extend(label)
+                    # tr_outputs.extend(list(output))
+                    # tr_labels.extend(label)
 
                 loss.backward()
                 self.optimizer.step()
@@ -164,11 +165,10 @@ class SupervisedTrainer:
             self.logger.add_scalar('epoch_metrics/epoch', epoch, epoch)
             self.logger.add_scalar('epoch_metrics/train_loss', train_loss, epoch)
 
-            outputs, ground_truth, valid_loss, users, items = self.predict(valid_dataloader)
+            outputs, ground_truth, valid_loss, users, items = self.predict(valid_dataloader, low_mem=True)
 #            with open(join(self.train_output_log, f"valid_output_{epoch}.log"), "w") as f:
 #                f.write("\n".join([f"label:{str(float(l))}, pred:{str(float(v))}" for v, l in zip(outputs, ground_truth)]))
-            results = calculate_metrics(ground_truth, outputs, users, items,
-                                        self.relevance_level, prediction_threshold=0.5, ranking_only=True)
+            results = calculate_metrics(ground_truth, outputs, users, items, self.relevance_level)
             results["loss"] = valid_loss
             results = {f"valid_{k}": v for k, v in results.items()}
             for k, v in results.items():
@@ -211,7 +211,7 @@ class SupervisedTrainer:
         outputs, ground_truth, valid_loss, internal_user_ids, internal_item_ids = self.predict(valid_dataloader)
         log_results(self.best_valid_output_path, ground_truth, outputs, internal_user_ids, internal_item_ids,
                     self.users, self.items)
-        results = calculate_metrics(ground_truth, outputs, internal_user_ids, internal_item_ids, self.relevance_level, 0.5)
+        results = calculate_metrics(ground_truth, outputs, internal_user_ids, internal_item_ids, self.relevance_level)
         results["loss"] = valid_loss
         results = {f"validation_{k}": v for k, v in results.items()}
         for k, v in results.items():
@@ -221,14 +221,14 @@ class SupervisedTrainer:
         outputs, ground_truth, test_loss, internal_user_ids, internal_item_ids = self.predict(test_dataloader)
         log_results(self.test_output_path, ground_truth, outputs, internal_user_ids, internal_item_ids,
                     self.users, self.items)
-        results = calculate_metrics(ground_truth, outputs, internal_user_ids, internal_item_ids, self.relevance_level, 0.5)
+        results = calculate_metrics(ground_truth, outputs, internal_user_ids, internal_item_ids, self.relevance_level)
         results["loss"] = test_loss
         results = {f"test_{k}": v for k, v in results.items()}
         for k, v in results.items():
             self.logger.add_scalar(f'final_results/{k}', v)
         print(f"\nTest results - best epoch {self.best_epoch}: {results}")
 
-    def predict(self, eval_dataloader):
+    def predict(self, eval_dataloader, low_mem=False):
         # bring models to evaluation mode
         self.model.eval()
 
@@ -270,7 +270,8 @@ class SupervisedTrainer:
                 outputs.extend(output.squeeze().tolist())
                 user_ids.extend(batch[
                                     INTERNAL_USER_ID_FIELD].squeeze().tolist())
-                item_ids.extend(batch[INTERNAL_ITEM_ID_FIELD].squeeze().tolist())
+                if not low_mem:
+                    item_ids.extend(batch[INTERNAL_ITEM_ID_FIELD].squeeze().tolist())
 
                 postprocess_time = time.perf_counter() - start_time - prepare_time - process_time
                 pbar.set_description(
--- SBR/utils/metrics.py
+++ SBR/utils/metrics.py
@@ -2,79 +2,66 @@
 import json
 from collections import defaultdict
 
-# import pytrec_eval
+import pytrec_eval
 from sklearn.metrics import ndcg_score
 import numpy as np
-from sklearn.metrics import precision_score, recall_score, f1_score
 
 from SBR.utils.statics import INTERNAL_USER_ID_FIELD, INTERNAL_ITEM_ID_FIELD
-
-# ranking_metrics = [
-#     "P_1",  # min number of pos interaction for users in test is 1, so P@higher than 1 may penalty even if it shouldn't
-#     "P_5",
-#     "P_10",
-#     "P_20",
-#     "ndcg_cut_5",
-#     "ndcg_cut_10",
-#     "ndcg_cut_20",
-#     "recall_100",
-#     "recall_1000",
-#     "Rprec",
-#     "recip_rank"
-# ]
 
 ranking_metrics = [
     "ndcg_cut_5",
     "ndcg_cut_10",
     "ndcg_cut_20",
+    "P_1",
+    "recip_rank"
 ]
 
-def calculate_metrics(ground_truth, prediction_scores, users, items,
-                      relevance_level=1, prediction_threshold=0.5, ranking_only=False):
-    # prediction_scores is sigmoid applied already.
-    result = calculate_ranking_metrics_macro_avg_over_users(ground_truth, prediction_scores, users, items, relevance_level)
-    # if ranking_only:
-    #     return result
-    #
-    # predictions = (prediction_scores > prediction_threshold).float()
-    #
-    # temp = calculate_cl_metrics_micro(ground_truth, predictions, prediction_threshold)
-    # result.update(temp)
-    #
-    # temp = calculate_cl_metrics_macro_avg_over_users(ground_truth, predictions, users, prediction_threshold)
-    # result.update(temp)
 
-    return result
-
-# todo later simplify these many functions, depending on which metrics to implement
-def calculate_ranking_metrics_macro_avg_over_users(ground_truth, prediction_scores, users, items, relevance_level,
-                                                   given_ranking_metrics=None):
+def calculate_metrics(ground_truth, prediction_scores, users, items, relevance_level, given_ranking_metrics=None):
     # # qid= user1:{ item1:1 } ...
     gt = {str(u): {} for u in set(users)}
     pd = {str(u): {} for u in set(users)}
+    min_not_zero = 1
     for i in range(len(ground_truth)):
-        gt[str(users[i])][str(items[i])] = float(ground_truth[i])
-        pd[str(users[i])][str(items[i])] = float(prediction_scores[i])
-    return calculate_ranking_metrics(gt, pd, relevance_level, given_ranking_metrics)
+        if len(items) == 0:
+            gt[str(users[i])][str(i)] = float(ground_truth[i])
+            pd[str(users[i])][str(i)] = float(prediction_scores[i])
+        else:
+            gt[str(users[i])][str(items[i])] = float(ground_truth[i])
+            pd[str(users[i])][str(items[i])] = float(prediction_scores[i])
+        if ground_truth[i] != 0 and ground_truth[i] < min_not_zero:
+            min_not_zero = ground_truth[i]
+    return calculate_ranking_metrics_macro_avg_over_users(gt, pd, relevance_level, given_ranking_metrics,
+                                                          True if min_not_zero!=1 else False)
 
 
-# def calculate_ranking_metrics(gt, pd, relevance_level, given_ranking_metrics=None):
-#     '''
-#
-#     :param gt: dict of user -> item -> true score (relevance)
-#     :param pd: dict of user -> item -> predicted score
-#     :param relevance_level:
-#     :param given_ranking_metrics:
-#     :return: metric scores
-#     '''
-#     if given_ranking_metrics is None:
-#         given_ranking_metrics = ranking_metrics
-#     evaluator = pytrec_eval.RelevanceEvaluator(gt, given_ranking_metrics, relevance_level=int(relevance_level))
-#     per_user_scores = evaluator.evaluate(pd)
-#     scores = [[metrics_dict.get(m, -1) for m in given_ranking_metrics] for metrics_dict in per_user_scores.values()]
-#     scores = np.array(scores).mean(axis=0).tolist()
-#     scores = dict(zip(given_ranking_metrics, scores))
-#     return scores
+def calculate_ranking_metrics_macro_avg_over_users(gt, pd, relevance_level,
+                                                   given_ranking_metrics=None, weighted_label=False):
+    if given_ranking_metrics is None:
+        given_ranking_metrics = ranking_metrics
+    if weighted_label:
+        # weighted ground truth
+        results = calculate_ndcg(gt, pd, [m for m in given_ranking_metrics if m.startswith("ndcg_")])
+    else:
+        gt = {k: {k2: int(v2) for k2, v2 in v.items()} for k, v in gt.items()}
+        results = calculate_ranking_metrics_pytreceval(gt, pd, relevance_level, given_ranking_metrics)
+    return results
+
+
+def calculate_ranking_metrics_pytreceval(gt, pd, relevance_level, given_ranking_metrics):
+    '''
+    :param gt: dict of user -> item -> true score (relevance)
+    :param pd: dict of user -> item -> predicted score
+    :param relevance_level:
+    :param given_ranking_metrics:
+    :return: metric scores
+    '''
+    evaluator = pytrec_eval.RelevanceEvaluator(gt, given_ranking_metrics, relevance_level=int(relevance_level))
+    per_user_scores = evaluator.evaluate(pd)
+    scores = [[metrics_dict.get(m, -1) for m in given_ranking_metrics] for metrics_dict in per_user_scores.values()]
+    scores = np.array(scores).mean(axis=0).tolist()
+    scores = dict(zip(given_ranking_metrics, scores))
+    return scores
 
 
 def ndcg(gt, pd, k):
@@ -86,22 +73,7 @@ def ndcg(gt, pd, k):
     return per_user_socre
 
 
-# def precision_k_macro_users_weighted(gt, pd, k):
-#     max_gt = 1
-#     for items in gt.values():
-#         for scores in items.values():
-#             if max(scores) > max_gt:
-#                 max_gt = max(scores)
-#     if max_gt > 1:
-#         raise ValueError("This is not implemented for the ratings dataset, have to set a relevance-level ...")
-#
-#     scores = []
-#     for user, items in gt.items():
-#         gt_items = sorted(items.values(), reverse=True)
-# todo contiue, it is a bit weird, as we have relevance threshold 0.5 also, and if gt=0.1 and pd=0.5 what's the deal ...
-
-
-def calculate_ranking_metrics(gt, pd, relevance_level, given_ranking_metrics=None):
+def calculate_ndcg(gt, pd, given_ranking_metrics):
     '''
 
     :param gt: dict of user -> item -> true score (relevance)
@@ -110,59 +82,14 @@ def calculate_ranking_metrics(gt, pd, relevance_level, given_ranking_metrics=Non
     :param given_ranking_metrics:
     :return: metric scores
     '''
-    if given_ranking_metrics is None:
-        given_ranking_metrics = ranking_metrics
     ret = defaultdict()
     for m in given_ranking_metrics:
         if m.startswith("ndcg_cut_"):
             per_user_scores = ndcg(gt, pd, int(m[m.rindex("_")+1:]))
-        # elif m.startswith("P_"):
-        #     score = precision_k_macro_users_weighted(gt, pd, int(m[m.rindex("_") + 1:]))
         else:
             raise NotImplementedError("other metrics not implemented")
         ret[m] = np.array(per_user_scores).mean().tolist()
     return ret
-
-
-# def get_p_r_f1(ground_truth, predictions, avg="binary"):
-#     return [precision_score(ground_truth, predictions, zero_division=0, average=avg), \
-#            recall_score(ground_truth, predictions, average=avg), \
-#            f1_score(ground_truth, predictions, average=avg)]
-
-
-# def calculate_cl_metrics_micro(ground_truth, predictions, prediction_threshold):
-#     return calculate_cl_micro(ground_truth, predictions)
-
-
-# def calculate_cl_micro(ground_truth, predictions):
-#     '''
-#     :param ground_truth: true classes (relevance)
-#     :param predictions: predicted relevance
-#     :return: micro p,r,f1
-#     '''
-#     scores = get_p_r_f1(ground_truth, predictions)
-#     return {"precision_micro": scores[0], "recall_micro": scores[1], "f1_micro": scores[2]}
-
-
-# def calculate_cl_metrics_macro_avg_over_users(ground_truth, predictions, users, prediction_threshold):
-#     gt_user = {u: [] for u in set(users)}
-#     pd_user = {u: [] for u in set(users)}
-#     for i in range(len(ground_truth)):
-#         gt_user[users[i]].append(int(ground_truth[i]))
-#         pd_user[users[i]].append(int(predictions[i]))
-#
-#     return calculate_cl_macro(gt_user, pd_user)
-
-
-# def calculate_cl_macro(gt_user, pd_user):
-#     '''
-#     :param gt_user: per user true class (relevant or not)
-#     :param pd_user: per user predicted class
-#     :return: macro p,r,f1
-#     '''
-#     scores = {u: get_p_r_f1(gt_user[u], pd_user[u]) for u in gt_user.keys()}
-#     scores = np.array(list(scores.values())).mean(axis=0).tolist()
-#     return {"precision_macro": scores[0], "recall_macro": scores[1], "f1_macro": scores[2]}
 
 
 def log_results(output_path, ground_truth, prediction_scores, internal_user_ids, internal_items_ids,
@@ -176,7 +103,7 @@ def log_results(output_path, ground_truth, prediction_scores, internal_user_ids,
     gt = {str(u): {} for u in sorted(set(user_ids))}
     pd = {str(u): {} for u in sorted(set(user_ids))}
     for i in range(len(ground_truth)):
-        gt[str(user_ids[i])][str(item_ids[i])] = float(ground_truth[i])  #TODO float?
+        gt[str(user_ids[i])][str(item_ids[i])] = float(ground_truth[i])
         pd[str(user_ids[i])][str(item_ids[i])] = float(prediction_scores[i])
     json.dump({"predicted": pd}, open(output_path['predicted'], 'w'))
     json.dump({"ground_truth": gt}, open(output_path['ground_truth'], 'w'))
