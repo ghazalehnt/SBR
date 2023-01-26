f4c22aae53fa211af604c36ebc5e9f7f13258839
Ghazaleh H. Torbati <ghazaleh@mpi-inf.mpg.de>
2023-01-26 08:49:08 +0100

    calc only for items in test set

--- SBR/scripts/precompute_item_item_BM25_ranking.py
+++ SBR/scripts/precompute_item_item_BM25_ranking.py
@@ -17,7 +17,7 @@ import time
 ITEM_ID_FIELD = "item_id"
 USER_ID_FIELD = "user_id"
 item_user_inter_text_fields = ["title", "category", "description"]
-
+topn = 1000
 
 def tokenize_function_torchtext(samples, tokenizer=None, doc_desc_field="text"):
     samples[f"tokenized_{doc_desc_field}"] = [tokenizer(text) for text in samples[doc_desc_field]]
@@ -30,9 +30,15 @@ def rank_items(item_id, query):
     f = open(join(dataset_path, "BM25_item_ranking" ,f"{item_id}.pkl"), 'wb')
     document_scores = index.get_scores(query)
     document_scores = np.argsort(document_scores)[::-1]
-    pickle.dump([doc_ids[doc] for doc in document_scores], f)
+    pickle.dump([doc_ids[doc] for doc in document_scores[:topn]], f)
     f.close()
     return
+
+
+def get_test_items(dataset_path):
+    test_dataset = pd.read_csv(os.path.join(dataset_path, f"test.csv"), dtype=str)
+    return set(test_dataset["item_id"])
+
 
 if __name__ == "__main__":
     random.seed(42)
@@ -42,6 +48,7 @@ if __name__ == "__main__":
     dataset_path = args.dataset_folder
     os.makedirs(join(dataset_path, "BM25_item_ranking"), exist_ok=True)
 
+    to_calc_items = get_test_items(dataset_path)
     use_col = [ITEM_ID_FIELD]
     use_col.extend(item_user_inter_text_fields)
     item_info = pd.read_csv(os.path.join(dataset_path, "items.csv"), dtype=str, usecols=use_col)
@@ -67,6 +74,8 @@ if __name__ == "__main__":
 
     start = time.time()
     pool = mp.Pool(mp.cpu_count())
-    pool.starmap(rank_items, [(item_id, item_text) for item_id, item_text in zip(item_info["item_id"], item_info["tokenized_text"])])
+
+    pool.starmap(rank_items, [(item_id, item_text) for item_id, item_text in \
+                              zip(item_info["item_id"], item_info["tokenized_text"]) if item_id in to_calc_items])
     pool.close()
     print(f"finished {time.time() - start} seconds")
