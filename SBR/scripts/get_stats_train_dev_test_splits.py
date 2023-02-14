612f4901f3fe4f4ee09cfb4c6a3ffdf7b6a6c358
Ghazaleh H. Torbati <ghazaleh@mpi-inf.mpg.de>
2023-02-14 16:59:00 +0100

    total ds stats

--- SBR/scripts/get_stats_train_dev_test_splits.py
+++ SBR/scripts/get_stats_train_dev_test_splits.py
@@ -173,6 +173,19 @@ if __name__ == '__main__':
     valid_unique_authors_per_user = valid.groupby(USER_ID_FIELD)[AUTHOR_FIELD].nunique().to_dict()
     test_unique_authors_per_user = test.groupby(USER_ID_FIELD)[AUTHOR_FIELD].nunique().to_dict()
 
+    # combined dataset stats:
+    temp = scipy.stats.describe(list({u: per_user['train'][u]+per_user['valid'][u]+per_user['test'][u] for u in per_user['train'].keys()}.values()))
+    statfile.write(f"TOTAL: stats for user interactions: {temp} - mean: {temp.mean.round(2)} - std: {math.sqrt(temp.variance).__round__(2)}\n")
+    temp = scipy.stats.describe(list({i: per_item['train'][i] + per_item['valid'][i] + per_item['test'][i] for i in per_item['train'].keys()}.values()))
+    statfile.write(f"TOTAL: stats for item interactions: {temp} - mean: {temp.mean.round(2)} - std: {math.sqrt(temp.variance).__round__(2)}\n")
+
+    temp = scipy.stats.describe(list({u: train_unique_authors_per_user[u]+valid_unique_authors_per_user[u]+test_unique_authors_per_user[u] for u in per_user['train'].keys()}.values()))
+    statfile.write(
+        f"TOTAL: avg num unique authors per user: {temp} - mean: {temp.mean.round(2)} - std: {math.sqrt(temp.variance).__round__(2)}\n")
+    temp = scipy.stats.describe(list({u: train_unique_users_per_author[u]+valid_unique_users_per_author[u]+test_unique_users_per_author[u] for u in per_user['train'].keys()}.values()))
+    statfile.write(
+        f"TOTAL: avg num unique users per author: {temp} - mean: {temp.mean.round(2)} - std: {math.sqrt(temp.variance).__round__(2)}\n\n\n")
+
     temp = scipy.stats.describe(list(per_user['train'].values()))
     statfile.write(f"TRAIN: stats for user interactions: {temp} - mean: {temp.mean.round(2)} - std: {math.sqrt(temp.variance).__round__(2)}\n")
     temp = scipy.stats.describe(list(per_item['train'].values()))
