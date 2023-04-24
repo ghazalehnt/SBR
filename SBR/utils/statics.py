aeb37558a432bf0adfa510756733731929f2256e
Ghazaleh H. Torbati <ghazaleh@mpi-inf.mpg.de>
2023-04-24 11:14:54 +0200

    diff num layers configurable

--- SBR/utils/statics.py
+++ SBR/utils/statics.py
@@ -1,2 +1,8 @@
 INTERNAL_USER_ID_FIELD = "internal_user_id"
 INTERNAL_ITEM_ID_FIELD = "internal_item_id"
+
+shorten_names = {
+    "item.title": "it",
+    "item.genres": "ig",
+    "item.description": "id"
+}
