28bfa65f1baf7347a6a65c35fcb597293a325b91
Ghazaleh H. Torbati <ghazaleh@mpi-inf.mpg.de>
2023-08-08 14:36:41 +0200

    saving topN scores

--- run_test_precalc_embeddings.py
+++ run_test_precalc_embeddings.py
@@ -1,11 +1,12 @@
 import argparse
 import json
-import os
-from collections import defaultdict
 from os.path import join
 
 import torch
 
+from SBR.utils.data_loading import load_split_dataset, get_user_used_items
+
+topN = 1000
 
 def main(result_folder):
 	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -14,22 +15,30 @@ def main(result_folder):
 	user_prec_reps = torch.load(join(result_folder, "user_prec_output"), map_location=device)
 	item_id_mapping = json.load(open(join(result_folder, "item_internal_ids.json"), 'r'))
 	user_id_mapping = json.load(open(join(result_folder, "user_internal_ids.json"), 'r'))
-
 	item_id_mapping_rev = {v: k for k, v in item_id_mapping.items()}
 
-	os.makedirs(join(result_folder, "per_user_all_items_scores_from_precalc"), exist_ok=True)
+	# reading train and valid to remove from items:
+	config = json.load(open(join(result_folder, "config.json"), 'r'))
+	datasets, user_info, item_info, _ = load_split_dataset(config["dataset"], False)
+	user_used_items = get_user_used_items(datasets, {})
+	not_include_in_test = user_used_items['train'].copy()
+	for user_id, u_items in user_used_items['validation'].items():
+		not_include_in_test[user_id] = not_include_in_test[user_id].union(u_items)
 
-	cnt = 0
+	results = {}
 	for user_exid, user_inid in user_id_mapping.items():
-		#results = defaultdict()
+		temp = {}
 		user_rep = user_prec_reps(torch.LongTensor([user_inid]).to(device))
 		res = torch.sigmoid(torch.sum(torch.mul(user_rep, item_prec_reps.weight), dim=1)).tolist()
-		#for item_inid in range(len(res)):
-		#	results[item_id_mapping_rev[item_inid]] = res[item_inid]
-		json.dump({user_exid: res}, open(join(result_folder, "per_user_all_items_scores_from_precalc", f"{user_exid}.json"), 'w'))
-		cnt += 1
-		if cnt % 1000 == 0:
-			print(f"{cnt} users done.")
+		for item_inid in range(len(res)):
+			if item_inid in not_include_in_test[user_inid]:
+				continue
+			temp[item_id_mapping_rev[item_inid]] = res[item_inid]
+		results[user_exid] = {k: v for k, v in sorted(temp.items(), key=lambda x: x[1], reverse=True)[:topN]}
+		if len(results.keys()) % 1000 == 0:
+			print(f"{len(results.keys())} users done.")
+
+	json.dump(results, open(join(result_folder, f"scores_sorted_{topN}_of_all_items_per_user.json"), 'w'))
 
 
 if __name__ == '__main__':
