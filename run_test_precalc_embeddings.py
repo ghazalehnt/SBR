b1d7df86753492423c96abfb37c4a3f7cb005c48
Ghazaleh H. Torbati <ghazaleh@mpi-inf.mpg.de>
2023-08-08 13:44:19 +0200

    save per user

--- run_test_precalc_embeddings.py
+++ run_test_precalc_embeddings.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import os
 from collections import defaultdict
 from os.path import join
 
@@ -16,15 +17,19 @@ def main(result_folder):
 
 	item_id_mapping_rev = {v: k for k, v in item_id_mapping.items()}
 
-	results = defaultdict(lambda: defaultdict())
+	os.makedirs(join(result_folder, "per_user_all_items_scores_from_precalc"), exist_ok=True)
+
+	cnt = 0
 	for user_exid, user_inid in user_id_mapping.items():
+		results = defaultdict()
 		user_rep = user_prec_reps(torch.LongTensor([user_inid]).to(device))
 		res = torch.sigmoid(torch.sum(torch.mul(user_rep, item_prec_reps.weight), dim=1)).tolist()
 		for item_inid in range(len(res)):
-			results[user_exid][item_id_mapping_rev[item_inid]] = res[item_inid]
-		if len(results.keys()) % 1000 == 0:
-			print(f"{len(results.keys())} users done.")
-	json.dump({"predicted": results}, open(join(result_folder, "all_items_scores_from_precalc.json"), 'w'))
+			results[item_id_mapping_rev[item_inid]] = res[item_inid]
+		json.dump({user_exid: results}, open(join(result_folder, f"{user_exid}.json"), 'w'))
+		cnt += 1
+		if cnt % 1000 == 0:
+			print(f"{cnt} users done.")
 
 
 if __name__ == '__main__':
