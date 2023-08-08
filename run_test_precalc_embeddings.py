04bafa30dd7a85ca0117235ba549f4d830dc1f4c
Ghazaleh Haratinezhad Torbati <ghazaleh@mpi-inf.mpg.de>
2023-08-08 13:13:33 +0200

    running all items for all users

--- run_test_precalc_embeddings.py
+++ run_test_precalc_embeddings.py
@@ -14,12 +14,16 @@ def main(result_folder):
 	item_id_mapping = json.load(open(join(result_folder, "item_internal_ids.json"), 'r'))
 	user_id_mapping = json.load(open(join(result_folder, "user_internal_ids.json"), 'r'))
 
+	item_id_mapping_rev = {v: k for k, v in item_id_mapping.items()}
+
 	results = defaultdict(lambda: defaultdict())
 	for user_exid, user_inid in user_id_mapping.items():
 		user_rep = user_prec_reps(torch.LongTensor([user_inid]).to(device))
-		for item_exid, item_inid in item_id_mapping.items():
-			item_rep = item_prec_reps(torch.LongTensor([item_inid]).to(device))
-			results[user_exid][item_exid] = torch.sigmoid(torch.sum(torch.mul(user_rep, item_rep), dim=1)).tolist()[0]
+		res = torch.sigmoid(torch.sum(torch.mul(user_rep, item_prec_reps.weight), dim=1)).tolist()
+		for item_inid in range(len(res)):
+			results[user_exid][item_id_mapping_rev[item_inid]] = res[item_inid]
+		if len(results.keys()) % 1000 == 0:
+			print(f"{len(results.keys())} users done.")
 	json.dump({"predicted": results}, open(join(result_folder, "all_items_scores_from_precalc.json"), 'w'))
 
 
